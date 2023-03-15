60faa8617c368fe8c2011a96cf7b2951d87cbf19
MarcSauratej <m.saura1dawnuria2020@gmail.com>
2023-03-15 17:43:42 +0100

    update view index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -159,8 +159,6 @@
                         </td>
                       @endforeach
                 </td>
-
-
                 <td class="px-6 py-4">
                     <a href="{{route('posts.delete',$post->id)}}" class="font-medium text-red-600 dark:text-blue-500 hover:underline">Delete Post</a>
                 </td>
